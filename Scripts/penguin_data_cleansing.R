1506a2dc47f9b9acb8a06316477fb2d95d6fc05a
evansjr5 <james.r.evans@vanderbilt.edu>
2024-10-21 12:45:17 -0500

    Final Cleaning Data

--- Scripts/penguin_data_cleansing.R
+++ Scripts/penguin_data_cleansing.R
@@ -12,7 +12,7 @@ vis_miss(penguins_lter_data)
 plot_missing(penguins_lter_data)
 
 # Clean data by removing rows with NA
-clean_data <- penguins_lter_data %>%
+clean_penguin <- penguins_lter_data %>%
   drop_na()
 
 # Visualize data after removing NAs
@@ -31,4 +31,39 @@ plot_correlation(clean_data)
 
 ## Scatterplot `price` with all other continuous features
 plot_scatterplot(split_columns(clean_data)$continuous, by = "Culmen.Length..mm.", sampled_rows = 1000L)
+
+ggplot(clean_data, aes(x=Body.Mass..g., y=Culmen.Depth..mm.,color = Species, shape = Island)) + 
+  geom_point() +
+  theme_classic() +
+  xlab("Body Mass (g)") +
+  ylab("Culmen Depth (mm)")
+ggsave(filename = "Depth.v.Bodymass_Species.png",dpi = 600)
+
+aov_res = aov(data = clean_data,formula = Culmen.Depth..mm. ~ Body.Mass..g.)
+
+model = lm(clean_data$Culmen.Depth..mm. ~ clean_data$Culmen.Length..mm.)
+
+ggplot(data = clean_data, aes(x = , y = Culmen.Depth..mm.)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  annotate("text",label = paste("R2 = ", round(summary(model)$r.squared, 2), "\n", 
+                                                   "p = ", round(summary(model)$coefficients[8], 4)))
+help(cor)
+
+
+for( i in penguins_lter_data$Individual.ID) {
+  len = str_length(i)
+  ss = substring(i,len)
+  if (ss == 1) {
+    penguins_lter_data$Sex == "Male"
+    }
+}
+
+
+# Assuming your DataFrame is named df
+filtered_df <- clean_penguin %>%
+  filter(Sex %in% c("MALE", "FEMALE") & Sex != "")
+
+write_csv(filtered_df,"clean_penguin_data.csv")
+
 
